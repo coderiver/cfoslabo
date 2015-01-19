a653bf6bca65116dd4f670ced3f85db69207f3e8
whats0n <mrdvv@mail.ru>
2015-01-19 15:49:56 +0200

    done

--- js/common.js
+++ js/common.js
@@ -15,5 +15,81 @@ head.ready(function() {
 	//     scrollFixedElements()
 	// });
 
-	console.log($('body').html());
+	//btn-to top
+	$('.js-btn-up').click(function() {
+		$('html, body').animate({scrollTop: $('html').offset().top}, 700);
+		return false;
+	});
+	//size
+	$('.fontsize-small').on('click', function() {
+		if ($(this).hasClass('is-active')) {
+		}
+		else {
+			$('.size__item').removeClass('is-active');
+			$(this).addClass('is-active');
+			$('body').addClass('is-small');
+			$('body').removeClass('is-medium');
+			$('body').removeClass('is-large');
+		};
+		return false;
+	});
+	$('.fontsize-medium').on('click', function() {
+		if ($(this).hasClass('is-active')) {
+		}
+		else {
+			$('.size__item').removeClass('is-active');
+			$(this).addClass('is-active');
+			$('body').addClass('is-medium');
+			$('body').removeClass('is-small');
+			$('body').removeClass('is-large');
+		};
+		return false;
+	});
+	$('.fontsize-large').on('click', function() {
+		if ($(this).hasClass('is-active')) {
+		}
+		else {
+			$('.size__item').removeClass('is-active');
+			$(this).addClass('is-active');
+			$('body').addClass('is-large');
+			$('body').removeClass('is-medium');
+			$('body').removeClass('is-small');
+		};
+		return false;
+	});
+	//color
+	$('.color-black').on('click', function () {
+		if ($(this).hasClass('is-active')) {
+		}
+		else {
+			$('.color__item').removeClass('is-active');
+			$(this).addClass('is-active');
+			$('body').addClass('is-black');
+			$('body').removeClass('is-blue');
+			$('body').removeClass('is-normal');
+		};
+		return false;
+	});
+	$('.color-blue').on('click', function () {
+		if ($(this).hasClass('is-active')) {
+		}
+		else {
+			$('.color__item').removeClass('is-active');
+			$(this).addClass('is-active');
+			$('body').addClass('is-blue');
+			$('body').removeClass('is-black');
+		};
+		return false;
+	});
+	$('.color-normal').on('click', function () {
+		if ($(this).hasClass('is-active')) {
+		}
+		else {
+			$('.color__item').removeClass('is-active');
+			$(this).addClass('is-active');
+			$('body').removeClass('is-blue');
+			$('body').removeClass('is-black');
+		};
+		return false;
+	});
 });
